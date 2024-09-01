782511747f37b6ba236d11a9b756ab489afeb43c
Richard <ricky201325@gmail.com>
2024-09-01 06:36:52 -0300

    Login terminado

--- login.py
+++ login.py
@@ -1,1 +1,11 @@
 print("Esto es un login")
+
+usuario = input("Ingrese su nombre de usuario: ")
+
+if usuario == "admin":
+    contrasena = input("Ingrese su contraseña: ")
+    if contrasena == "password":
+        print("Bienvenido administrador")
+    else:
+        print("Contraseña incorrecta")
+        

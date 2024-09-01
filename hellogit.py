a3fff22fef267c70501a7f0a23d1aaef227ae23c
Richard <ricky201325@gmail.com>
2024-09-01 00:57:51 -0300

    version 1

--- hellogit.py
+++ hellogit.py
@@ -1,8 +1,10 @@
 # Lista de números
 numeros = [10, 20, 30, 40, 50]
+numeros1 = [20, 30, 40, 50]
 
 # Calcular la suma de los números
 suma = sum(numeros)
+suma1 = sum(numeros)
 
 # Calcular el número de elementos
 cantidad = len(numeros)

dbac48bc0ae18e6019e5719a68493a89d8189e97
Richard <ricky201325@gmail.com>
2024-09-01 00:03:20 -0300

    Este es el tercer commit

---
+++ hellogit.py
@@ -0,0 +1,14 @@
+# Lista de números
+numeros = [10, 20, 30, 40, 50]
+
+# Calcular la suma de los números
+suma = sum(numeros)
+
+# Calcular el número de elementos
+cantidad = len(numeros)
+
+# Calcular el promedio
+promedio = suma / cantidad
+
+# Mostrar el resultado
+print("El promedio es:", promedio)
